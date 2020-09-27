d162bfadc700c0f40107859d446a47f59e4ceb92
Sourav Singh Rawat <aidenfrostbite@gmail.com>
2020-09-27 16:53:13 +0530

    JS added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,22 @@
 const pieceOfYourHeart = new Audio();
 pieceOfYourHeart.src = "/assets/tracks/piece_of_your_heart.mp3";
+const loseControl = new Audio();
+loseControl.src = "/assets/tracks/lose_control.mp3";
 
-function playSong(e) {
-  console.log(e);
+function playSong(ev) {
+  let name = ev.target.alt; // Piece of your heart
+  name = name.toLowerCase().split(" "); // piece of your heart
+  let i = 0;
+  let newName = name
+    .map((el) => {
+      if (i === 0) {
+        i++;
+        return el;
+      } else {
+        el = el.charAt(0).toUpperCase() + el.slice(1);
+        return el;
+      }
+    })
+    .join("");
+  eval(newName).play();
 }
