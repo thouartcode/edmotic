b064629cbd27b1a1fde8b03843528138c67368d4
Anugrah Pateriya <69234368+thouartcode@users.noreply.github.com>
2020-09-28 16:49:56 +0530

    dynamic html

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,18 +3,31 @@ pieceOfYourHeart.src = "/assets/tracks/piece_of_your_heart.mp3";
 const loseControl = new Audio();
 loseControl.src = "/assets/tracks/lose_control.mp3";
 
-let currentSong = undefined;
+let currentSong = undefined; // Currently playing song
 
-function playSong(ev) {
+const songs = {
+  pieceOfYourHeart: {
+    name: "Piece of your heart",
+    artist: "Meduza ft. Goodboys",
+    imagePath: "/assets/img/piece_of_your_heart.jpg",
+  },
+  loseControl: {
+    name: "Lose Control",
+    artist: "Meduza ft. Becky Hill and Goodboys",
+    imagePath: "/assets/img/lose_control.jpg",
+  },
+};
+
+// Play Song and stop the current if another is played
+function playSong(e) {
   if (currentSong !== undefined) {
     eval(currentSong).pause();
     eval(currentSong).currentTime = 0;
   }
 
-  let name = ev.target.alt; // Piece of your heart
-  name = name.toLowerCase().split(" "); // piece of your heart
+  let songName = e.target.alt.toLowerCase().split(" ");
   let i = 0;
-  let newName = name
+  let newSongName = songName
     .map((el) => {
       if (i === 0) {
         i++;
@@ -25,6 +38,34 @@ function playSong(ev) {
       }
     })
     .join("");
-  currentSong = newName;
-  eval(newName).play();
+  currentSong = newSongName;
+  eval(newSongName).play();
 }
+
+// Render the songs to the UI
+function renderUI() {
+  Object.keys(songs).forEach((el) => {
+    let newSong = el;
+    console.log(songs[el]);
+  });
+}
+
+function renderCard(document) {
+  const html = `
+    <li class="weekly__list--1 song__card">
+      <div class="song__card--cover" onclick="playSong(event)">
+        <img src="${document.imagePath}" alt="${document.name}" class="song__card--image">
+      </div>
+      <div class="song__card--name">${document.name}</div>
+      <div class="song__card--artist">${document.artist}</div>
+    </li>
+  `;
+}
+
+// 1. Loop through the object and find the songs
+// 2. Create the HTML and insert in into the DOM
+
+function init() {
+  renderUI();
+}
+init();
