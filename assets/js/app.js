ce904cd02a8d51182cf3e4d04b57715b42c08f5b
Anugrah Pateriya <69234368+thouartcode@users.noreply.github.com>
2020-09-29 16:16:12 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,12 +7,12 @@ const blahBlahBlah = new Audio();
 const tenFeetTall = new Audio();
 const bellaCiao = new Audio();
 
-pieceOfYourHeart.src = "/assets/tracks/piece_of_your_heart.mp3";
-loseControl.src = "/assets/tracks/lose_control.mp3";
-falling.src = "/assets/tracks/falling.mp3";
-blahBlahBlah.src = "/assets/tracks/blah_blah_blah.mp3";
-tenFeetTall.src = "/assets/tracks/ten_feet_tall.mp3";
-bellaCiao.src = "/assets/tracks/bella_ciao.mp3";
+pieceOfYourHeart.src = "./assets/tracks/piece_of_your_heart.mp3";
+loseControl.src = "./assets/tracks/lose_control.mp3";
+falling.src = "./assets/tracks/falling.mp3";
+blahBlahBlah.src = "./assets/tracks/blah_blah_blah.mp3";
+tenFeetTall.src = "./assets/tracks/ten_feet_tall.mp3";
+bellaCiao.src = "./assets/tracks/bella_ciao.mp3";
 
 /****************************************************************************/
 
@@ -28,32 +28,32 @@ const songs = {
   pieceOfYourHeart: {
     name: "Piece of your heart",
     artist: "Meduza ft. Goodboys",
-    imagePath: "/assets/img/piece_of_your_heart.jpg",
+    imagePath: "./assets/img/piece_of_your_heart.jpg",
   },
   loseControl: {
     name: "Lose Control",
     artist: "Meduza ft. Becky Hill and Goodboys",
-    imagePath: "/assets/img/lose_control.jpg",
+    imagePath: "./assets/img/lose_control.jpg",
   },
   falling: {
     name: "Falling",
     artist: "Alesso",
-    imagePath: "/assets/img/falling.jpg",
+    imagePath: "./assets/img/falling.jpg",
   },
   blahBlahBlah: {
     name: "Blah Blah Blah",
     artist: "Armin van Buuren",
-    imagePath: "/assets/img/blah_blah_blah.jpg",
+    imagePath: "./assets/img/blah_blah_blah.jpg",
   },
   tenFeetTall: {
     name: "Ten Feet Tall",
     artist: "Afrojack ft. Wrabel",
-    imagePath: "/assets/img/ten_feet_tall.jpg",
+    imagePath: "./assets/img/ten_feet_tall.jpg",
   },
   bellaCiao: {
     name: "Bella Ciao",
     artist: "Hardwell ft. Maddix",
-    imagePath: "/assets/img/bella_ciao.jpg",
+    imagePath: "./assets/img/bella_ciao.jpg",
   },
 };
 
@@ -94,7 +94,9 @@ function renderCard(doc) {
   const html = `
     <li class="weekly__list--1 song__card">
       <div class="song__card--cover" onclick="playSong(event)">
-        <img src="${doc.imagePath}" alt="${doc.name}" class="song__card--image">
+        <img src=".${doc.imagePath}" alt="${
+    doc.name
+  }" class="song__card--image">
       </div>
       <div class="song__card--name">${formatText(doc.name)}</div>
       <div class="song__card--artist">${doc.artist}</div>
@@ -119,10 +121,10 @@ function renderButtons() {
   let html;
   if (eval(currentSong) !== undefined) {
     elements.papl.innerHTML = "";
-    html = `<use xlink:href="/assets/img/sprite.svg#icon-pause" class="player__mc--glyph">`;
+    html = `<use xlink:href="./assets/img/sprite.svg#icon-pause" class="player__mc--glyph">`;
   } else {
     elements.papl.innerHTML = "";
-    html = `<use xlink:href="/assets/img/sprite.svg#icon-play" class="player__mc--glyph">`;
+    html = `<use xlink:href="./assets/img/sprite.svg#icon-play" class="player__mc--glyph">`;
   }
   elements.papl.insertAdjacentHTML("afterbegin", html);
 }
