7c1cff7d393a55783bf66291b5fb3502b003f98a
Anugrah Pateriya <69234368+thouartcode@users.noreply.github.com>
2020-09-29 16:07:19 +0530

    started adding player

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,26 @@
+/***************************AUDIOS*******************************************/
+
 const pieceOfYourHeart = new Audio();
-pieceOfYourHeart.src = "/assets/tracks/piece_of_your_heart.mp3";
 const loseControl = new Audio();
+const falling = new Audio();
+const blahBlahBlah = new Audio();
+const tenFeetTall = new Audio();
+const bellaCiao = new Audio();
+
+pieceOfYourHeart.src = "/assets/tracks/piece_of_your_heart.mp3";
 loseControl.src = "/assets/tracks/lose_control.mp3";
+falling.src = "/assets/tracks/falling.mp3";
+blahBlahBlah.src = "/assets/tracks/blah_blah_blah.mp3";
+tenFeetTall.src = "/assets/tracks/ten_feet_tall.mp3";
+bellaCiao.src = "/assets/tracks/bella_ciao.mp3";
+
+/****************************************************************************/
+
+const elements = {
+  weeklyList: document.querySelector(".weekly__list"),
+  papl: document.querySelector(".player__mc--holder"),
+  paplBtn: document.querySelector(".player__mc--papl"),
+};
 
 let currentSong = undefined; // Currently playing song
 
@@ -16,6 +35,26 @@ const songs = {
     artist: "Meduza ft. Becky Hill and Goodboys",
     imagePath: "/assets/img/lose_control.jpg",
   },
+  falling: {
+    name: "Falling",
+    artist: "Alesso",
+    imagePath: "/assets/img/falling.jpg",
+  },
+  blahBlahBlah: {
+    name: "Blah Blah Blah",
+    artist: "Armin van Buuren",
+    imagePath: "/assets/img/blah_blah_blah.jpg",
+  },
+  tenFeetTall: {
+    name: "Ten Feet Tall",
+    artist: "Afrojack ft. Wrabel",
+    imagePath: "/assets/img/ten_feet_tall.jpg",
+  },
+  bellaCiao: {
+    name: "Bella Ciao",
+    artist: "Hardwell ft. Maddix",
+    imagePath: "/assets/img/bella_ciao.jpg",
+  },
 };
 
 // Play Song and stop the current if another is played
@@ -26,6 +65,7 @@ function playSong(e) {
   }
 
   let songName = e.target.alt.toLowerCase().split(" ");
+
   let i = 0;
   let newSongName = songName
     .map((el) => {
@@ -40,32 +80,67 @@ function playSong(e) {
     .join("");
   currentSong = newSongName;
   eval(newSongName).play();
+  renderButtons();
 }
 
 // Render the songs to the UI
 function renderUI() {
   Object.keys(songs).forEach((el) => {
-    let newSong = el;
-    console.log(songs[el]);
+    renderCard(songs[el]);
   });
 }
 
-function renderCard(document) {
+function renderCard(doc) {
   const html = `
     <li class="weekly__list--1 song__card">
       <div class="song__card--cover" onclick="playSong(event)">
-        <img src="${document.imagePath}" alt="${document.name}" class="song__card--image">
+        <img src="${doc.imagePath}" alt="${doc.name}" class="song__card--image">
       </div>
-      <div class="song__card--name">${document.name}</div>
-      <div class="song__card--artist">${document.artist}</div>
+      <div class="song__card--name">${formatText(doc.name)}</div>
+      <div class="song__card--artist">${doc.artist}</div>
     </li>
   `;
+  elements.weeklyList.insertAdjacentHTML("beforeend", html);
 }
 
-// 1. Loop through the object and find the songs
-// 2. Create the HTML and insert in into the DOM
+// Format name
+function formatText(text) {
+  let newText = text.split("");
+  if (newText.length > 13) {
+    newText = newText.slice(0, 12);
+    newText = newText.join("");
+    return newText + "...";
+  }
+  return text;
+}
+
+// Render buttons
+function renderButtons() {
+  let html;
+  if (eval(currentSong) !== undefined) {
+    elements.papl.innerHTML = "";
+    html = `<use xlink:href="/assets/img/sprite.svg#icon-pause" class="player__mc--glyph">`;
+  } else {
+    elements.papl.innerHTML = "";
+    html = `<use xlink:href="/assets/img/sprite.svg#icon-play" class="player__mc--glyph">`;
+  }
+  elements.papl.insertAdjacentHTML("afterbegin", html);
+}
+
+// Event listeners
+function setupEventListeners() {
+  elements.paplBtn.addEventListener("click", () => {
+    if (eval(currentSong).paused) {
+      eval(currentSong).play();
+    } else {
+      eval(currentSong).pause();
+    }
+  });
+}
 
 function init() {
   renderUI();
+  renderButtons();
+  setupEventListeners();
 }
 init();
